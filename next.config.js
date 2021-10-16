a45c1d982271243cb36fd9ddc740f0dde2b7c821
Wingman <ryuash123@gmail.com>
2021-10-16 16:52:51 +0800

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '/'
+  assetPrefix: isProd ? '/sven-gallery/' : '',
 }
