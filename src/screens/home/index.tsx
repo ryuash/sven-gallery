b8fcee2e5c00f413a969554a7d39aa957e4434ad
Wingman <ryuash123@gmail.com>
2021-10-16 18:53:22 +0800

    .

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -20,25 +20,6 @@ const Home = () => {
       <SvgContainer>
         SVG HERE
       </SvgContainer>
-      {/* <Hamburger />
-      <Mailbox /> */}
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="286"
-        height="334"
-        fill="none"
-        viewBox="0 0 286 334"
-      >
-        <ShieldPart
-          d="M 143, 333 C 31.09 261.823 1 73.61 1 73.61 L 143 1 v 332 z"
-          color="#A78BFA"
-        />
-        <ShieldPart
-          d="M 143, 333 C 254.911 261.823 285 73.61 285 73.61 L 143 1 v 332 z"
-          color="#8B5CF6"
-        />
-        <Checkmark />
-      </svg> */}
     </BaseContainer>
   )
 }
