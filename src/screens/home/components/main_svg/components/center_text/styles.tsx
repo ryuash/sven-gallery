9e77e50f26e10e0c4a4235aef5a872d78be4b000
Wingman <ryuash123@gmail.com>
2021-10-16 21:48:15 +0800

    add: flicker animation

---
+++ src/screens/home/components/main_svg/components/center_text/styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const FlickerCss = styled.path`
+  animation: flicker 3s linear infinite;
+
+  @keyframes flicker {
+    0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100% {
+      opacity: .99;
+    }
+    20%, 21.999%, 63%, 63.999%, 65%, 69.999% {
+      opacity: 0.4;
+    }
+  }
+`;
