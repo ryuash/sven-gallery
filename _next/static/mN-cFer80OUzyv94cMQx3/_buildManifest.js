ff3bd855abe72d0decfd673c131367aff74975c0
ryuash <ryuash@users.noreply.github.com>
2021-10-17 14:05:41 +0000

    deploy: 883e068d1345afb6fffab32abc77562cd5e000da

--- _next/static/mN-cFer80OUzyv94cMQx3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/694-808aca55ddedb9fa2da7.js","static/chunks/pages/index-191b98162de2dd6f211f.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
